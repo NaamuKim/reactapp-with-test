3a04280d5d5ae0802c2f42f44de3dc13679bc79c
Jiwon Kim <lost731@naver.com>
2022-01-29 17:22:37 +0900

    Add: plus, minus, on/off button with test-codes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import { useCallback, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+  const onClickPlus = useCallback(() => {
+    setCounter((count) => count + 1);
+  }, []);
+  const onClickMinus = useCallback(() => {
+    setCounter((count) => count - 1);
+  }, []);
+  const onClickOnOff = useCallback(() => {
+    setDisabled((prev) => !prev);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h3 data-testid='counter'>{counter}</h3>
+        <div>
+          <button data-testid='minus-button' disabled={disabled} onClick={onClickMinus}>
+            -
+          </button>
+          <button data-testid='plus-button' disabled={disabled} onClick={onClickPlus}>
+            +
+          </button>
+          <button style={{ backgroundColor: 'blue' }} data-testid='on/off-button' onClick={onClickOnOff}>
+            on/off
+          </button>
+        </div>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,53 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('the counter starts at 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-  const lintTest = screen.getByRole('button', {
-    name: 'lintTest',
-  });
-  expect(lintTest).toHaveTextContent('linkTest');
+  // 스크린 오브젝트를 이용하여 원하는 엘리멘트에 접근
+  const $counter = screen.getByTestId('counter');
+  expect($counter).toHaveTextContent(0);
+});
+
+test('minus button has correct text', () => {
+  render(<App />);
+  const $minusBtn = screen.getByTestId('minus-button');
+  expect($minusBtn).toHaveTextContent('-');
+});
+
+test('plus button has correct text', () => {
+  render(<App />);
+  const $plusBtn = screen.getByTestId('plus-button');
+  expect($plusBtn).toHaveTextContent('+');
+});
+
+test('When the + button prerssed, the counter changes to 1', () => {
+  render(<App />);
+  const $button = screen.getByTestId('plus-button');
+  fireEvent.click($button);
+  const $counter = screen.getByTestId('counter');
+  expect($counter).toHaveTextContent(1);
+});
+
+test('When the - button prerssed, the counter changes to -1', () => {
+  render(<App />);
+  const $button = screen.getByTestId('minus-button');
+  fireEvent.click($button);
+  const $counter = screen.getByTestId('counter');
+  expect($counter).toHaveTextContent(-1);
+});
+
+test('on/off button has bllue color', () => {
+  render(<App />);
+  const $button = screen.getByTestId('on/off-button');
+  expect($button).toHaveStyle({ backgroundColor: 'blue' });
+});
+
+test('Prevent the -,+ button from being pressed when the on/off button is clicked', () => {
+  render(<App />);
+  const $onoffBtn = screen.getByTestId('on/off-button');
+  const $plusBtn = screen.getByTestId('plus-button');
+  const $minusBtn = screen.getByTestId('minus-button');
+  fireEvent.click($onoffBtn);
+  expect($plusBtn).toBeDisabled();
+  expect($minusBtn).toBeDisabled();
 });
